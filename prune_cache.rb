460e5a4d49fa30fd84dba7359d7faf10113f2b45
Ivan Schneider <contact@ivanschneider.fr>
2018-04-21 22:05:03 +0200

    Max 20 cache directories

--- prune_cache.rb
+++ prune_cache.rb
@@ -4,7 +4,7 @@ puts 'Pruning cached assets'
 
 caches = Dir["#{ARGV.first}/assets-*"]
 
-while caches.size > 2
+while caches.size > 20
   lru_cache = caches.min_by { |directory| IO.read "#{directory}/last_use" }
   puts "Prune directory #{lru_cache}"
   FileUtils.remove_dir lru_cache
