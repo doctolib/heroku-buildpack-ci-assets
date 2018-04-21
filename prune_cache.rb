6616b60b472ce45613f5eb48d68c905d5e76c38a
Ivan Schneider <contact@ivanschneider.fr>
2018-04-21 21:27:57 +0200

    Prune cache

--- prune_cache.rb
+++ prune_cache.rb
@@ -1,1 +1,12 @@
-puts ARGV.inspect
+require 'fileutils'
+
+puts 'Pruning cached assets'
+
+caches = Dir["#{ARGV.first}/assets-*"]
+
+while caches.size > 2
+  lru_cache = caches.min_by { |directory| IO.read "#{directory}/last_use" }
+  puts "Prune directory #{lru_cache}"
+  FileUtils.remove_dir lru_cache
+  caches.delete lru_cache
+end
